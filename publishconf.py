931f5ea721a257310b0f6278e1faf47a9b22213c
luketk97 <taekun@ualberta.ca>
2021-02-10 17:34:23 -0700

    add docs

--- publishconf.py
+++ publishconf.py
@@ -10,14 +10,13 @@ sys.path.append(os.curdir)
 from pelicanconf import *
 
 # If your site is available via HTTPS, make sure SITEURL begins with https://
-SITEURL = 'https://luketk97.github.io/cmput404lab5/'
+SITEURL = 'https://luketk97.github.io/cmput404lab5'
 RELATIVE_URLS = False
 
 FEED_ALL_ATOM = 'feeds/all.atom.xml'
 CATEGORY_FEED_ATOM = 'feeds/{slug}.atom.xml'
 
 OUTPUT_PATH = "docs/"
-DELETE_OUTPUT_DIRECTORY = True
 
 # Following items are often useful when publishing
 
